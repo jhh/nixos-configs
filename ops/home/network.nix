5b240075a7724426638d78674f76d1deb5b6a9cb
Jeff Hutchison <jeff@jeffhutchison.com>
2021-08-29 10:38:28 -0400

    Pin packages

--- ops/home/network.nix
+++ ops/home/network.nix
@@ -1,7 +1,16 @@
 # ops/home/network.nix
-
+let
+  # Pin the deployment package-set to a specific version of nixpkgs
+  pkgs = import (builtins.fetchGit {
+    name = "nixos-21.05";
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "release-21.05";
+    rev = "ba6124b6214db87261c71bd80e09e345d30d4af1";
+  }) {};
+in
 {
   network = {
+    inherit pkgs;
     description = "Home Lab";
   };
 
