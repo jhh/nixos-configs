c8ce5836c5e66ca5373afc00e8bcc5cfd45855de
Jeff Hutchison <jeff@jeffhutchison.com>
2021-08-29 15:46:47 -0400

    Remove cruft

--- common/home.nix
+++ common/home.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 {
-
   home.sessionVariables = {
     EDITOR = "vim";
     MANWIDTH = 100;
@@ -11,6 +10,7 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
   programs.fish = {
     enable = true;
     plugins = [
@@ -82,19 +82,4 @@
       relativenumber = true;
     };
   };
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "jeff";
-  home.homeDirectory = "/home/jeff";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.11";
 }
