b787c8fe423b0db0ef40bf26a695e51cd7794b58
Jeff Hutchison <jeff@jeffhutchison.com>
2021-08-30 12:48:31 -0400

    Refactor fish config into separate file

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,3 +1,4 @@
 [
+  ./fish.nix
   ./git.nix
 ]
---
+++ home/programs/fish.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }: {
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "colored-man";
+        src = pkgs.fetchFromGitHub {
+          owner = "decors";
+          repo = "fish-colored-man";
+          rev = "1ad8fff696d48c8bf173aa98f9dff39d7916de0e";
+          sha256 = "0l32a5bq3zqndl4ksy5iv988z2nv56a91244gh8mnrjv45wpi1ms";
+          fetchSubmodules = true;
+        };
+      }
+
+      {
+        name = "fzf";
+        src = pkgs.fetchFromGitHub {
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "176c8465b0fad2d5c30aacafff6eb5accb7e3826";
+          sha256 = "16mdfyznxjhv7x561srl559misn37a35d2q9fspxa7qg1d0sc3x9";
+          fetchSubmodules = true;
+        };
+      }
+
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "45a9ff6d0932b0e9835cbeb60b9794ba706eef10";
+          sha256 = "1kjyl4gx26q8175wcizvsm0jwhppd00rixdcr1p7gifw6s308sd5";
+          fetchSubmodules = true;
+        };
+      }
+    ];
+
+    shellAbbrs = { gst = "git status"; };
+
+    shellAliases = {
+      dc = "docker-compose";
+      dps = "docker-compose ps";
+      dcd = "docker-compose down --remove-orphans";
+      drm = "docker images -a -q | xargs docker rmi -f";
+      du = "ncdu --color dark -rr -x";
+      ping = "prettyping";
+    };
+  };
+}
