aabdfeb6f4789a81c51f7406a4cfa13a156509e6
Jeff Hutchison <jeff@jeffhutchison.com>
2021-08-30 09:57:32 -0400

    Jazz up fish shell

--- common/home.nix
+++ common/home.nix
@@ -15,13 +15,24 @@
     enable = true;
     plugins = [
       {
+        name = "colored-man";
+        src = pkgs.fetchFromGitHub {
+          owner = "decors";
+          repo = "fish-colored-man";
+          rev = "1ad8fff696d48c8bf173aa98f9dff39d7916de0e";
+          sha256 = "0l32a5bq3zqndl4ksy5iv988z2nv56a91244gh8mnrjv45wpi1ms";
+          fetchSubmodules = true;
+        };
+      }
+
+      {
         name = "fzf";
         src = pkgs.fetchFromGitHub {
-          "owner" = "PatrickF1";
-          "repo" = "fzf.fish";
-          "rev" = "176c8465b0fad2d5c30aacafff6eb5accb7e3826";
-          "sha256" = "16mdfyznxjhv7x561srl559misn37a35d2q9fspxa7qg1d0sc3x9";
-          "fetchSubmodules" = true;
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "176c8465b0fad2d5c30aacafff6eb5accb7e3826";
+          sha256 = "16mdfyznxjhv7x561srl559misn37a35d2q9fspxa7qg1d0sc3x9";
+          fetchSubmodules = true;
         };
       }
 
@@ -72,6 +83,11 @@
   programs.man = {
     enable = true;
     generateCaches = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
   };
 
   programs.vim = {
