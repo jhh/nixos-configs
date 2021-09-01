203b2784a4cbc9794f1d3840ea57ddf4578a2ef7
Jeff Hutchison <jeff@jeffhutchison.com>
2021-09-01 15:19:15 -0400

    Add additional which-key maps

--- home/programs/nvim/keymaps.lua
+++ home/programs/nvim/keymaps.lua
@@ -2,12 +2,15 @@ local wk = require("which-key")
 
 wk.register({
   f = {
-    name = "File", -- optional group name
+    name = "Find", -- optional group name
+    b = { "<cmd>Telescope buffers<cr>", "Find Buffer" },
     f = { "<cmd>Telescope find_files<cr>", "Find File" },
+    g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help Tag" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File"}
   },
   g = {
     name = "Git",
     g = { "<cmd>LazyGit<cr>", "LazyGit"},
   },
-}, { prefix = "<leader>" })
+}, { prefix = " " })
