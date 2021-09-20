e1d0bca9710c7aab1ca88ff25249254301706573
Jeff Hutchison <jeff@jeffhutchison.com>
2021-09-20 07:11:30 -0400

    Change tmux key bindings

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -3,8 +3,8 @@ let
   tmuxConf = ''
     bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
     set -g status-left-length 50
-    bind-key j choose-tree -swZ
-    bind C-j display-popup -E "\
+    bind-key a choose-tree -swZ
+    bind C-a display-popup -E "\
       tmux list-sessions -F '#{?session_attached,,#{session_name}}' |\
       sed '/^$/d' |\
       fzf --reverse --header jump-to-session --preview 'tmux capture-pane -pt {}'  |\
@@ -13,9 +13,10 @@ let
 in {
   programs.tmux = {
     enable = true;
-    shortcut = "s";
     baseIndex = 1;
     extraConfig = tmuxConf;
+    keyMode = "vi";
+    shortcut = "s";
     plugins = with pkgs.tmuxPlugins; [
       nord # https://github.com/arcticicestudio/nord-tmux
       pain-control # https://github.com/tmux-plugins/tmux-pain-control
