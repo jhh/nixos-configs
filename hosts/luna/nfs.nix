7ab0d6b397d8b0e07823a16349eee9b3eb8ad6e4
Jeff Hutchison <jeff@jeffhutchison.com>
2021-09-24 13:03:49 -0400

    Share homelab over NFS

--- hosts/luna/nfs.nix
+++ hosts/luna/nfs.nix
@@ -3,9 +3,10 @@
   services.nfs.server = {
     enable = true;
     exports = ''
-      /mnt/tank/media/plex  192.168.1.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
-      /mnt/tank/proxmox/pbs 192.168.1.47(rw,insecure,sync,no_subtree_check)
-      /mnt/tank/proxmox/pve 192.168.1.20(rw,insecure,sync,no_subtree_check,no_root_squash) 192.168.1.21(rw,insecure,sync,no_subtree_check,no_root_squash)
+      /mnt/tank/share/homelab 192.168.1.0/24(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/media/plex    192.168.1.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
+      /mnt/tank/proxmox/pbs   192.168.1.47(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/proxmox/pve   192.168.1.20(rw,insecure,sync,no_subtree_check,no_root_squash) 192.168.1.21(rw,insecure,sync,no_subtree_check,no_root_squash)
     '';
   };
 
