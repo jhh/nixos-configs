4cf33efcffb15ef4bb3cd0bfa14c0f44e83ac4ca
Jeff Hutchison <jeff@jeffhutchison.com>
2021-10-12 08:05:07 -0400

    Update session selection key binding

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -3,8 +3,8 @@ let
   tmuxConf = ''
     bind-key r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
     set -g status-left-length 50
-    bind-key a choose-tree -swZ
-    bind C-a display-popup -E "\
+    bind-key f choose-tree -swZ
+    bind C-f display-popup -E "\
       tmux list-sessions -F '#{?session_attached,,#{session_name}}' |\
       sed '/^$/d' |\
       fzf --reverse --header jump-to-session --preview 'tmux capture-pane -pt {}'  |\
