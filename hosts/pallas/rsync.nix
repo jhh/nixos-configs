181ba3e8cee049d7b1ce1b734717e417817bbd0c
Jeff Hutchison <jeff@jeffhutchison.com>
2021-11-03 08:17:50 -0400

    Use tailscale address for luna

--- hosts/pallas/rsync.nix
+++ hosts/pallas/rsync.nix
@@ -5,12 +5,13 @@
     startAt = "hourly";
 
     environment = {
+      BACKUP_HOST = "luna.ts.j3ff.io";
       RSYNC_RSH = "${pkgs.openssh}/bin/ssh";
     };
 
     script = ''
       echo backing up /home/jeff
-      ${pkgs.rsync}/bin/rsync -az /home/jeff jeff@luna:backup/pallas/
+      ${pkgs.rsync}/bin/rsync -az /home/jeff jeff@$BACKUP_HOST:backup/pallas/
     '';
   };
 
