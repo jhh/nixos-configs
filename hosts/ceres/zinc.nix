6603ee251ec574970d3a39ea594686bac1662424
Jeff Hutchison <jeff@jeffhutchison.com>
2021-11-07 14:31:04 -0500

    Add more sandbox

--- hosts/ceres/zinc.nix
+++ hosts/ceres/zinc.nix
@@ -3,19 +3,45 @@ let
   zinc = flakes.zinc.defaultPackage.x86_64-linux;
 in
 {
+  users.users.zinc = {
+    isSystemUser = true;
+    shell = null;
+    description = "Zinc Service";
+    group = "zinc";
+  };
+  users.groups.zinc = { };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "zinc" ];
+      commands = [{
+        command = "${pkgs.zrepl}/bin/zrepl status --mode=raw";
+        options = [ "NOPASSWD" ];
+      }];
+    }
+  ];
+
   systemd.services.zinc = {
     description = "Zrepl local console";
     documentation = [ "https://github.com/jhh/zinc" ];
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.zrepl ];
     environment = {
       FLASK_APP = "zinc";
       FLASK_ENV = "production";
+      SUDO = "${config.security.wrapperDir}/sudo";
+      ZREPL = "${pkgs.zrepl}/bin/zrepl";
     };
     serviceConfig = {
+      User = "zinc";
+      Group = "zinc";
       ExecStart = "${zinc}/bin/flask run --host=0.0.0.0";
       Restart = "always";
+
+      # security hardening
+      PrivateTmp = "true";
+      ProtectSystem = "true";
+      ProtectHome = "true";
     };
   };
 }
