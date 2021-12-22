3a63ede3b024425603fd306a3aec1e7137f3c3b4
Jeff Hutchison <jeff@jeffhutchison.com>
2021-12-22 12:07:03 -0500

    Remove ceres from prometheus

--- hosts/eris/prometheus.nix
+++ hosts/eris/prometheus.nix
@@ -15,7 +15,7 @@
         static_configs = [{
           targets = [
             "eris:9002"
-            "ceres:9002"
+            # "ceres:9002"
             "luna:9002"
             "phobos:9002"
             "pve-01:9100"
@@ -54,7 +54,7 @@
         static_configs = [
           {
             targets = [
-              "ceres:9811"
+              # "ceres:9811"
               "eris:9811"
               "luna:9811"
               "phobos:9811"
