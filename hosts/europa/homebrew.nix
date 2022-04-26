4fc2f9bed14e3d8d6157f2c1c7e7b6efd588f45c
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-26 10:12:24 -0400

    Add mas apps

--- hosts/europa/homebrew.nix
+++ hosts/europa/homebrew.nix
@@ -5,14 +5,23 @@
     enable = true;
     cleanup = "zap";
 
-    brews = [
-      "mas"
-    ];
+    # brews = [
+    # ];
 
     casks = [
       "visual-studio-code"
     ];
 
-    masApps = [ ];
+    masApps = {
+      "1Password 7" = 1333542190;
+      "Affinity Photo" = 824183456;
+      "Infuse 7" = 1136220934;
+      "Paprika Recipe Manager 3" = 1303222628;
+      "Pcalc" = 403504866;
+      "Reeder 5" = 1529448980;
+      "Slack for Desktop" = 803453959;
+      "Tailscale" = 1475387142;
+      "Twitter" = 1482454543;
+    };
   };
 }
