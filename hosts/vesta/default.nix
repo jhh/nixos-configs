69e6b537aecf3d2e17a15b2f780e3da9d10986d5
Jeff Hutchison <jeff@jeffhutchison.com>
2022-04-27 08:07:30 -0400

    Add docker to vesta

--- hosts/vesta/default.nix
+++ hosts/vesta/default.nix
@@ -46,6 +46,8 @@
     qemuGuest.enable = true;
   };
 
+  virtualisation.docker.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
