d606ec604746be93b335beebd704949ba2e3df50
Jeff Hutchison <jeff@jeffhutchison.com>
2022-05-10 06:33:58 -0400

    Add picom to ceres

--- hosts/ceres/default.nix
+++ hosts/ceres/default.nix
@@ -72,6 +72,14 @@
     displayManager.defaultSession = "xfce";
   };
 
+  services.picom = {
+    enable = true;
+    fade = true;
+    inactiveOpacity = 0.9;
+    shadow = true;
+    fadeDelta = 4;
+  };
+
   services.gnome.gnome-keyring.enable = true;
 
   sound.enable = true;
