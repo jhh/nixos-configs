70289da53b14e500d499997eea0f35714cf36f93
Jeff Hutchison <jeff@jeffhutchison.com>
2022-06-09 14:59:04 -0400

    Move shell init

--- common/home-manager/fish/default.nix
+++ common/home-manager/fish/default.nix
@@ -60,7 +60,26 @@ in
     plugins = defaultPlugins ++ darwinPlugins;
 
     # fzf.fish plugin ctrl-R keybind is overwritten by vanilla fzf, so rebind
-    interactiveShellInit = builtins.readFile ./interactiveShellInit.fish;
+    # interactiveShellInit = builtins.readFile ./interactiveShellInit.fish;
+    interactiveShellInit = ''
+      bind \cr _fzf_search_history
+
+      if not functions -q __direnv_export_eval; and command -sq direnv
+          direnv hook fish | source
+      end
+
+      set --local WPI_YEAR 2022
+      if test -d ~/wpilib/$WPI_YEAR
+          abbr --add --global pathweaver ~/wpilib/$WPI_YEAR/tools/PathWeaver.py
+          abbr --add --global outlineviewer ~/wpilib/$WPI_YEAR/tools/OutlineViewer.py
+          set --export JAVA_HOME ~/wpilib/$WPI_YEAR/jdk
+          fish_add_path $JAVA_HOME/bin
+      end
+
+      if test -x /opt/homebrew/bin/brew
+          eval (/opt/homebrew/bin/brew shellenv)
+      end
+    '';
 
     # shellInit =
     #   lib.optionalString pkgs.stdenv.isDarwin ''
