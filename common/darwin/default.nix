083580e1e6a55f2a7a2f8c3a171ebcf71bf20777
Jeff Hutchison <jeff@jeffhutchison.com>
2022-06-14 17:03:35 -0400

    Add some darwin config

--- common/darwin/default.nix
+++ common/darwin/default.nix
@@ -5,4 +5,17 @@
     ./homebrew.nix
     ./sudo-touch.nix
   ];
+
+  system.defaults.dock = {
+    autohide = true;
+    mru-spaces = false;
+    orientation = "left";
+  };
+
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+
 }
