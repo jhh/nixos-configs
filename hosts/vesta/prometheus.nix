024f254d858690d4404f5ae6244d2328dd9bb893
Jeff Hutchison <jeff@jeffhutchison.com>
2022-06-23 17:56:34 -0400

    Add nut to prom

--- hosts/vesta/prometheus.nix
+++ hosts/vesta/prometheus.nix
@@ -6,119 +6,132 @@
     "100.124.210.30" = [ "docker-02" ];
   };
 
-  services.prometheus = {
-    enable = true;
-    port = 9001;
-    webExternalUrl = "http://vesta.lan.j3ff.io:${toString config.services.prometheus.port}";
-
-    scrapeConfigs = [
-      {
-        job_name = "node";
-        static_configs = [{
-          targets = [
-            "docker-01:9100"
-            "docker-02:9100"
-            "eris:9002"
-            # "ceres:9002"
-            "luna:9002"
-            "phobos:9002"
-            "pve-01:9100"
-            "pve-02:9100"
-            "pve-03:9100"
-            "pve-11:9100"
-            "ups-01:9100"
-          ];
-        }];
-      }
-      {
-        job_name = "grafana";
-        static_configs = [{
-          targets = [
-            "grafana.j3ff.io:80"
-          ];
-        }];
-      }
-      {
-        job_name = "prometheus";
-        static_configs = [{
-          targets = [
-            "vesta:9001"
-          ];
-        }];
-      }
-      {
-        job_name = "pihole";
-        static_configs = [
-          {
-            targets = [
-              "docker-01:9617"
-            ];
-          }
-        ];
-      }
-      {
-        job_name = "zrepl";
-        static_configs = [
-          {
-            targets = [
-              # "ceres:9811"
-              # "eris:9811"
-              "luna:9811"
-              "phobos:9811"
-            ];
-          }
-        ];
-      }
-    ];
-
-    alertmanagers = [{
-      static_configs = [{
-        targets = [ "localhost:9093" ];
-      }];
-    }];
-
-    rules = [
-      ''
-        groups:
-         - name: default
-           rules:
-           - alert: InstanceDown
-             expr: up == 0
-             for: 1m
-           - alert: ServiceFail
-             expr: node_systemd_units{state="failed"} > 0
-      ''
-    ];
-
-    alertmanager = {
+  services.prometheus =
+    {
       enable = true;
-      webExternalUrl = "http://vesta.ts.j3ff.io:9093";
-      configuration = {
-        global = {
-          smtp_smarthost = "localhost:25";
-          smtp_from = "alertmanager@j3ff.io";
-        };
-        route = {
-          receiver = "pushover";
-        };
-        receivers = [
-          {
-            name = "email";
-            email_configs = [{
-              to = "jeff@j3ff.io";
-              require_tls = false;
-            }];
-          }
+      port = 9001;
+      webExternalUrl = "http://vesta.lan.j3ff.io:${toString config.services.prometheus.port}";
 
-          {
-            name = "pushover";
-            pushover_configs = [{
-              user_key = "ugdx1vs5quycvqg3stin54ps5jqm3i";
-              token = "ag9wqe95khtkxz1t7rjxvhtxdmfmpv";
-            }];
-          }
-        ];
+      scrapeConfigs = [
+        {
+          job_name = "node";
+          static_configs = [{
+            targets = [
+              "docker-01:9100"
+              "docker-02:9100"
+              "eris:9002"
+              # "ceres:9002"
+              "luna:9002"
+              "phobos:9002"
+              "pve-01:9100"
+              "pve-02:9100"
+              "pve-03:9100"
+              "pve-11:9100"
+              "ups-01:9100"
+            ];
+          }];
+        }
+        {
+          job_name = "grafana";
+          static_configs = [{
+            targets = [
+              "grafana.j3ff.io:80"
+            ];
+          }];
+        }
+        {
+          job_name = "prometheus";
+          static_configs = [{
+            targets = [
+              "vesta:9001"
+            ];
+          }];
+        }
+        {
+          job_name = "pihole";
+          static_configs = [
+            {
+              targets = [
+                "docker-01:9617"
+              ];
+            }
+          ];
+        }
+        {
+          job_name = "ups";
+          metrics_path = "/ups_metrics";
+          params = { ups = [ "ups" ]; };
+          static_configs = [
+            {
+              targets = [
+                "docker-01:9199"
+              ];
+            }
+          ];
+        }
+        {
+          job_name = "zrepl";
+          static_configs = [
+            {
+              targets = [
+                # "ceres:9811"
+                # "eris:9811"
+                "luna:9811"
+                "phobos:9811"
+              ];
+            }
+          ];
+        }
+      ];
+
+      alertmanagers = [{
+        static_configs = [{
+          targets = [ "localhost:9093" ];
+        }];
+      }];
+
+      rules = [
+        ''
+          groups:
+           - name: default
+             rules:
+             - alert: InstanceDown
+               expr: up == 0
+               for: 1m
+             - alert: ServiceFail
+               expr: node_systemd_units{state="failed"} > 0
+        ''
+      ];
+
+      alertmanager = {
+        enable = true;
+        webExternalUrl = "http://vesta.ts.j3ff.io:9093";
+        configuration = {
+          global = {
+            smtp_smarthost = "localhost:25";
+            smtp_from = "alertmanager@j3ff.io";
+          };
+          route = {
+            receiver = "pushover";
+          };
+          receivers = [
+            {
+              name = "email";
+              email_configs = [{
+                to = "jeff@j3ff.io";
+                require_tls = false;
+              }];
+            }
+
+            {
+              name = "pushover";
+              pushover_configs = [{
+                user_key = "ugdx1vs5quycvqg3stin54ps5jqm3i";
+                token = "ag9wqe95khtkxz1t7rjxvhtxdmfmpv";
+              }];
+            }
+          ];
+        };
       };
     };
-  };
 }
