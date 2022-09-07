a39ed26128f24d096b3a3df0c87b62fa2dba971a
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-07 16:37:37 -0400

    Add pve-1 to allowed pve NFS

--- hosts/luna/nfs.nix
+++ hosts/luna/nfs.nix
@@ -7,7 +7,7 @@
       /mnt/tank/share/homelab   192.168.1.0/24(rw,insecure,sync,no_subtree_check)
       /mnt/tank/media/plex      192.168.1.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
       /mnt/tank/proxmox/pbs     192.168.1.47(rw,insecure,sync,no_subtree_check)
-      /mnt/tank/proxmox/pve     -rw,insecure,sync,no_subtree_check,no_root_squash 192.168.1.20 192.168.1.21 192.168.1.22
+      /mnt/tank/proxmox/pve     -rw,insecure,sync,no_subtree_check,no_root_squash 192.168.1.20 192.168.1.21 192.168.1.22 10.1.0.9
     '';
   };
 
