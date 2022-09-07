481d1e40881de725852f4831de90dae8793a84a3
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-07 08:23:58 -0400

    Fix rclone path for music

--- hosts/luna/rclone.nix
+++ hosts/luna/rclone.nix
@@ -8,7 +8,7 @@
     startAt = "*-*-* 03:20:00";
 
     script = ''
-      RCLONE_OPTS="--config ${config.age.secrets.rclone_conf.path} --skip-links --fast-list --transfers=32"
+      RCLONE_OPTS="--config ${config.age.secrets.rclone_conf.path} --skip-links --fast-list --transfers=32 -v"
 
       echo backing up /mnt/tank/backup/git
       ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/backup/git b2:j3ff-git
@@ -17,7 +17,7 @@
       ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/share/jeff b2:j3ff-home/jeff/
 
       echo backing up /mnt/tank/media/plex/music
-      ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/media/plex/music b2:j3ff-music/iTunes/Music
+      ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/media/plex/music/Music b2:j3ff-music/iTunes/Music/
     '';
   };
 
