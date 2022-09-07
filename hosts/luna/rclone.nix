cb3deb23bc86c5890567a01efd72d57c1957a375
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-07 08:08:40 -0400

    Add rclone opts

--- hosts/luna/rclone.nix
+++ hosts/luna/rclone.nix
@@ -8,11 +8,16 @@
     startAt = "*-*-* 03:20:00";
 
     script = ''
+      RCLONE_OPTS="--config ${config.age.secrets.rclone_conf.path} --skip-links --fast-list --transfers=32"
+
       echo backing up /mnt/tank/backup/git
-      ${pkgs.rclone}/bin/rclone --config ${config.age.secrets.rclone_conf.path} --skip-links sync /mnt/tank/backup/git b2:j3ff-git
+      ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/backup/git b2:j3ff-git
 
       echo backing up /mnt/tank/share/jeff
-      ${pkgs.rclone}/bin/rclone --config ${config.age.secrets.rclone_conf.path} --skip-links sync /mnt/tank/share/jeff b2:j3ff-home/jeff/
+      ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/share/jeff b2:j3ff-home/jeff/
+
+      echo backing up /mnt/tank/media/plex/music
+      ${pkgs.rclone}/bin/rclone $RCLONE_OPTS sync /mnt/tank/media/plex/music b2:j3ff-music/iTunes/Music
     '';
   };
 
