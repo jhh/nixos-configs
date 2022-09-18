f119ed8ee4a2ced7d38a4109fc9c565ea31e31f1
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-18 11:48:27 -0400

    Add Gitea to eris

--- hosts/eris/gitea.nix
+++ hosts/eris/gitea.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }:
 {
-  services.rpcbind.enable = true;
-  systemd.mounts = [{
-    type = "nfs";
-    mountConfig = {
-      Options = "noatime";
-    };
-    what = "10.1.0.7:/mnt/tank/services/gitea";
-    where = "/var/lib/gitea";
-  }];
+  # services.rpcbind.enable = true;
+  # systemd.mounts = [{
+  #   type = "nfs";
+  #   mountConfig = {
+  #     Options = "noatime";
+  #   };
+  #   what = "10.1.0.7:/mnt/tank/services/gitea";
+  #   where = "/var/lib/gitea";
+  #   before = [ "systemd-tmpfiles-setup.service" ];
+  # }];
+
+  services.gitea = {
+    enable = true;
+    domain = "j3ff.io";
+    rootUrl = "http://eris.ts.j3ff.io:3000/";
+    database.type = "postgres";
+
+  };
 }
