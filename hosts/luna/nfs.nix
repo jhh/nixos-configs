80aa8a2e291ece33565239384afa12be5b57b36d
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-18 12:42:43 -0400

    Export git backup dir in NFS

--- hosts/luna/nfs.nix
+++ hosts/luna/nfs.nix
@@ -4,6 +4,7 @@
     enable = true;
     exports = ''
       /mnt/tank/backup/postgres 10.1.0.0/24(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/backup/git      10.1.0.0/24(rw,insecure,sync,no_subtree_check)
       /mnt/tank/share/homelab   192.168.1.0/24(rw,insecure,sync,no_subtree_check)
       /mnt/tank/media/plex      192.168.1.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
       /mnt/tank/proxmox/pbs     192.168.1.47(rw,insecure,sync,no_subtree_check)
