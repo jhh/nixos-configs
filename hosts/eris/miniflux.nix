e571ded1708c03c5ec83c867061d4771e314dda3
Jeff Hutchison <jeff@jeffhutchison.com>
2022-09-26 13:15:02 -0400

    Proxy miniflux through nginx

--- hosts/eris/miniflux.nix
+++ hosts/eris/miniflux.nix
@@ -8,9 +8,22 @@
   services.miniflux = {
     enable = true;
     config = {
-      LISTEN_ADDR = "0.0.0.0:8010";
+      LISTEN_ADDR = "127.0.0.1:8010";
+      BASE_URL = "https://miniflux.j3ff.io";
     };
     adminCredentialsFile = config.age.secrets.miniflux_secret.path;
   };
 
+  services.nginx.virtualHosts."miniflux.j3ff.io" = {
+    # security.acme is configured for eris globally in nginx.nix
+    forceSSL = true;
+    enableACME = true;
+    acmeRoot = null;
+
+    locations = {
+      "/" = {
+        proxyPass = "http://127.0.0.1:8010";
+      };
+    };
+  };
 }
