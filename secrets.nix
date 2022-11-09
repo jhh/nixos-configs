3f4836a197cb9b3d75c5561c317fe9b2a60525bf
Jeff Hutchison <jeff@jeffhutchison.com>
2022-11-09 10:31:26 -0500

    Update ceres ssh public key for agenix

--- secrets.nix
+++ secrets.nix
@@ -4,7 +4,7 @@ let
   vesta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGgUVfRwIOyNKefOH6zXIPthvvEu/xObNRG6nSS1ht8";
   luna = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUJb6IP7qsp/FPbtVKl1CbX2lOYQDjDcgV0c5qAJv9W";
   phobos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBEldSjroPbKUueasCQuy88nE9X9Wt1a4lSbd3XSzvps";
-  ceres = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIagSXjSePjzj3d3GdUlH8Xe50CQbBNXLiazCCgIH6mI";
+  ceres = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFzrT7SvMHVmqP9olCUcS4WsCy4xnJ41RXdPNK8KDRrG";
   eris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEv0m+EZsTedPMjzq+a/9rl2c3iAdOKwnFQfGLFHb4y4";
 
   hosts = [ vesta luna phobos ceres eris ];
