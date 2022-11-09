bc50af03c66d1084e171347bf787402a3fbfcb18
Jeff Hutchison <jeff@jeffhutchison.com>
2022-11-09 17:42:47 -0500

    Add sabnzbd to ceres

--- hosts/ceres/media.nix
+++ hosts/ceres/media.nix
@@ -1,4 +1,9 @@
 { config, pkgs, ... }:
+let
+  mediaUser = "media";
+  mediaGroup = "media";
+  sabnzbdDataDir = "/var/lib/sabnzbd";
+in
 {
 
   fileSystems."/mnt/media/tv" = {
@@ -6,10 +11,36 @@
     fsType = "nfs";
   };
 
+  # sonarr
+
   services.sonarr = {
     enable = true;
-    user = "media";
+    user = mediaUser;
+    group = mediaGroup;
   };
+
+  # sabnzbd
+
+  systemd.tmpfiles.rules = [
+    "d '${sabnzbdDataDir}' 0700 ${mediaUser} ${mediaGroup} - -"
+  ];
+
+  systemd.services.sabnzbd = {
+    description = "SABnzbd binary newsreader";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = mediaUser;
+      Group = mediaGroup;
+      ExecStart = "${pkgs.sabnzbd}/bin/sabnzbd --config-file ${sabnzbdDataDir}/sabnzbd.ini --disable-file-log --logging 1 --browser 0";
+      Restart = "on-failure";
+    };
+  };
+
+  # nginx
+
 
   services.nginx = {
     enable = true;
@@ -18,10 +49,20 @@
     recommendedGzipSettings = true;
   };
 
-  services.nginx.virtualHosts."sonarr.j3ff.io" = {
-    locations = {
-      "/" = {
-        proxyPass = "http://127.0.0.1:8989";
+  services.nginx.virtualHosts = {
+    "sonarr.j3ff.io" = {
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:8989";
+        };
+      };
+    };
+
+    "sabnzbd.j3ff.io" = {
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:8080";
+        };
       };
     };
   };
