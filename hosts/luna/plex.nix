4d8d2d0fff950be09c2066db64b1753bd10f916e
Jeff Hutchison <jeff@jeffhutchison.com>
2022-11-19 09:40:58 -0500

    Add plex to media group

--- hosts/luna/plex.nix
+++ hosts/luna/plex.nix
@@ -1,9 +1,12 @@
 { config, pkgs, ... }:
 {
   fileSystems."/var/lib/plex" =
-    { device = "rpool/local/plex";
+    {
+      device = "rpool/local/plex";
       fsType = "zfs";
     };
+
+  users.users.plex.extraGroups = [ "media" ];
 
   services.plex = {
     enable = true;
