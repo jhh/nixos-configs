51ca86951429d0f6103c71d9db2aa55f759a54e9
Jeff Hutchison <jeff@jeffhutchison.com>
2022-12-01 08:11:44 -0500

    Update root passwd

--- common/users/root.nix
+++ common/users/root.nix
@@ -5,7 +5,7 @@
 {
   users.users.root = {
     # https://start.1password.com/open/i?a=7Z533SZAYZCNVL764G5INOV75Q&v=lwpxghrefna57cr6nw7mr3bybm&i=v6cyausjzre6hjypvdsfhlkbty&h=my.1password.com
-    hashedPassword = "$6$fQkrR0Y/UOZ7$RG1ARltUwRE2Q/zSM88en2KNpx2gDfzV/PSqjCq/c3njspl62.h6HFPnk1L.b8UvSWnxvoew/r79/CxwfzUYW1";
+    hashedPassword = "$y$j9T$6B8V0Z9VkFiU0fMwSuLrA0$z3YHuwwAZro3N7TopVIsNltIJ5BXt3TQj1wQqt5HSuD";
     openssh.authorizedKeys.keys = config.users.users.jeff.openssh.authorizedKeys.keys;
   };
 
