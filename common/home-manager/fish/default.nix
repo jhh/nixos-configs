8425a698a0ac2263b0fbd353e156465cb4489e83
Jeff Hutchison <jeff@jeffhutchison.com>
2022-12-17 10:42:06 -0500

    Add workaround to fix nix paths

--- common/home-manager/fish/default.nix
+++ common/home-manager/fish/default.nix
@@ -62,6 +62,13 @@ in
     # fzf.fish plugin ctrl-R keybind is overwritten by vanilla fzf, so rebind
     # interactiveShellInit = builtins.readFile ./interactiveShellInit.fish;
     interactiveShellInit = ''
+      ### Add nix binary paths to the PATH
+      # Perhaps someday will be fixed in nix or nix-darwin itself
+      if test (uname) = Darwin
+        fish_add_path --prepend --path --move --global "$HOME/.nix-profile/bin" /nix/var/nix/profiles/default/bin /run/current-system/sw/bin
+      end
+
+      # fzf.fish plugin ctrl-R keybind is overwritten by vanilla fzf, so rebind
       bind \cr _fzf_search_history
 
       if not functions -q __direnv_export_eval; and command -sq direnv
