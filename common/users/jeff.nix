1f68f6bdb3970522716fdfb5dc754dc90461cfb9
Jeff Hutchison <jeff@jeffhutchison.com>
2023-01-05 07:55:08 -0500

    Add jeff to strykeforce group

--- common/users/jeff.nix
+++ common/users/jeff.nix
@@ -9,7 +9,7 @@
     shell = pkgs.fish;
     home = "/home/jeff";
     description = "Jeff Hutchison";
-    extraGroups = [ "docker" "wheel" ];
+    extraGroups = [ "docker" "strykeforce" "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqpWpNJzfzioGYyR9q4wLwPkBrnmc/Gdl6JsO+SUpel jeff@j3ff.io" ];
   };
 
