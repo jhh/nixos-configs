e6bcba9126663cf1d881b3c29518355185458e90
Jeff Hutchison <jeff@jeffhutchison.com>
2023-12-01 10:35:00 -0500

    Fix vendorSha256 deprecation warning

--- common/modules/watchtower/nut-exporter.nix
+++ common/modules/watchtower/nut-exporter.nix
@@ -26,7 +26,7 @@ in
               sha256 = "sha256-fymVx6FJGII2PmWXVfeCRTxfO+35bmyn/9iL0iPuBgo=";
             };
 
-            vendorSha256 = "sha256-ji8JlEYChPBakt5y6+zcm1l04VzZ0/fjfGFJ9p+1KHE=";
+            vendorHash = "sha256-ji8JlEYChPBakt5y6+zcm1l04VzZ0/fjfGFJ9p+1KHE=";
           };
       in
       {
