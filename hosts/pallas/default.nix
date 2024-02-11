3b4250fc5a859badf47074034de8b32400ed6218
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-11 10:05:31 -0500

    Migrate pallas to CT

--- hosts/pallas/default.nix
+++ hosts/pallas/default.nix
@@ -1,53 +1,22 @@
-{ strykeforce, config, pkgs, ... }:
+{ strykeforce, config, modulesPath, pkgs, ... }:
 let
-
   strykeforce-manage = strykeforce.packages.${pkgs.system}.manage;
 in
 {
 
   imports = [
-    ./hardware-configuration.nix
+    (modulesPath + "/virtualisation/proxmox-lxc.nix")
     ./postgresql.nix
     ./strykeforce-website.nix
   ];
 
   boot.tmp.cleanOnBoot = true;
-  zramSwap.enable = true;
 
   environment.systemPackages = with pkgs; [
     bat
-    git
     nixfmt
     strykeforce-manage
   ];
-
-  networking = {
-    hostName = "pallas";
-    domain = "lan.j3ff.io";
-    useDHCP = false;
-
-    interfaces.ens18 = {
-      useDHCP = false;
-      ipv4.addresses = [{
-        address = "10.1.0.47";
-        prefixLength = 24;
-      }];
-    };
-
-    defaultGateway = {
-      address = "10.1.0.1";
-      interface = "ens18";
-    };
-
-    nameservers = [ "1.1.1.1" "1.0.0.1" "8.8.8.8" ];
-    firewall.enable = false;
-  };
-
-  services = {
-    fstrim.enable = true;
-    qemuGuest.enable = true;
-  };
-
 
   system.stateVersion = "21.11";
 }
