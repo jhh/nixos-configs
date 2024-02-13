1e39710b8f779f1a8202a588b2d0e722e7e001b0
Jeff Hutchison <jeff@jeffhutchison.com>
2024-02-12 20:39:04 -0500

    Clean up luna NFS exports

--- hosts/luna/nfs.nix
+++ hosts/luna/nfs.nix
@@ -6,11 +6,10 @@
       /mnt/tank/backup/homeassistant 10.1.0.0/24(rw,insecure,sync,no_subtree_check)
       /mnt/tank/backup/postgres      10.1.0.0/24(rw,insecure,sync,no_subtree_check)
       /mnt/tank/backup/gitea         10.1.0.0/24(rw,insecure,sync,no_subtree_check,no_root_squash)
-      /mnt/tank/share/homelab        192.168.1.0/24(rw,insecure,sync,no_subtree_check)
-      /mnt/tank/media/plex           192.168.1.0/24(rw,insecure,sync,no_subtree_check,crossmnt) 10.1.0.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
-      /mnt/tank/proxmox/pbs          192.168.1.47(rw,insecure,sync,no_subtree_check)
-      /mnt/tank/proxmox/pve          -rw,insecure,sync,no_subtree_check,no_root_squash 192.168.1.20 192.168.1.21 192.168.1.22 10.1.0.0/24
-      /mnt/tank/services/gitea  1    0.1.0.0/24(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/share/homelab        10.1.0.0/24(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/media/plex           10.1.0.0/24(rw,insecure,sync,no_subtree_check,crossmnt) 10.1.0.0/24(rw,insecure,sync,no_subtree_check,crossmnt)
+      /mnt/tank/proxmox/pbs          10.1.0.9(rw,insecure,sync,no_subtree_check)
+      /mnt/tank/proxmox/pve          -rw,insecure,sync,no_subtree_check,no_root_squash 10.1.0.9 10.1.0.11
     '';
   };
 
