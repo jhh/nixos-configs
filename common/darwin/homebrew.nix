946c18011831332b44235ad361b14ac2630a80e1
Jeff Hutchison <jeff@jeffhutchison.com>
2024-08-15 11:28:56 -0400

    Remove Affinity apps

--- common/darwin/homebrew.nix
+++ common/darwin/homebrew.nix
@@ -35,8 +35,6 @@
     ];
 
     masApps = {
-      "Affinity Photo" = 824183456;
-      "Affinity Designer" = 824171161;
       "Icon Slate" = 439697913;
       "Infuse 7" = 1136220934;
       "Paprika Recipe Manager 3" = 1303222628;
