eae78f88bda0dbf69c93721d3597e181eb946c91
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-10 08:12:08 -0500

    Disable troublesome nvim plugins

--- common/home-manager/nvim/default.nix
+++ common/home-manager/nvim/default.nix
@@ -73,11 +73,11 @@ let
 
   vimPluginsWithConfig = with pkgs.vimPlugins;
     map pluginWithConfig [
-      nvim-lspconfig # https://github.com/neovim/nvim-lspconfig
+      # nvim-lspconfig # https://github.com/neovim/nvim-lspconfig
       nvim-cmp # https://github.com/hrsh7th/nvim-cmp
       treesitter # https://github.com/nvim-treesitter/nvim-treesitter
       telescope-nvim # https://github.com/nvim-telescope/telescope.nvim
-      which-key-nvim # https://github.com/folke/which-key.nvim
+      # which-key-nvim # https://github.com/folke/which-key.nvim
     ];
 in
 {
