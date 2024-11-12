218a4d5d0210c205f250630c11e623acd592099e
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-12 11:13:05 -0500

    Use latest version of nix instead of git version

--- common/default.nix
+++ common/default.nix
@@ -45,7 +45,7 @@
     nixpkgs.config.allowUnfree = true;
 
     nix = {
-      package = pkgs.nixVersions.git;
+      package = pkgs.nixVersions.latest;
       extraOptions = ''
         experimental-features = nix-command flakes
         cores = 0
