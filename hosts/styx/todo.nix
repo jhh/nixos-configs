a4927fdce659faffd64de6f47bdbc6ef96c53944
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-14 12:31:38 -0500

    Add secrets to todo service

--- hosts/styx/todo.nix
+++ hosts/styx/todo.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }: {
 
+  age.secrets.todo_secrets = {
+    file = ../../secrets/todo_secrets.age;
+  };
+
   services.todo = {
     enable = true;
+    secrets = [ config.age.secrets.todo_secrets.path ];
   };
 }
