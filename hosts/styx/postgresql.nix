c2418d44ba22f7a7eb2d38a5cb0ec4720dc6d1ee
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-14 12:37:02 -0500

    Configure database for todo service

--- hosts/styx/postgresql.nix
+++ hosts/styx/postgresql.nix
@@ -3,5 +3,12 @@
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_16;
+    ensureDatabases = [ "todo" ];
+    ensureUsers = [
+      {
+        name = "todo";
+        ensureDBOwnership = true;
+      }
+    ];
   };
 }
