92bfaeb65b7dac99ddea870cd935006d094f8f98
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-15 06:51:52 -0500

    Enable pgadmin on styx

--- hosts/styx/postgresql.nix
+++ hosts/styx/postgresql.nix
@@ -1,7 +1,30 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
 
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_16;
   };
+
+  age.secrets.pgadmin_passwd = lib.mkIf config.services.pgadmin.enable {
+    file = ../../secrets/pgadmin_passwd.age;
+    owner = "pgadmin";
+    group = "pgadmin";
+  };
+
+  services.pgadmin = {
+    enable = true;
+    initialEmail = "jeff@j3ff.io";
+    initialPasswordFile = "${config.age.secrets.pgadmin_passwd.path}";
+  };
+
+  # enabled in todo.nix
+  services.nginx.virtualHosts."pgadmin.j3ff.io" = lib.mkIf config.services.pgadmin.enable {
+    locations = {
+      "/" = {
+        proxyPass = "http://127.0.0.1:5050";
+      };
+    };
+  };
+
+
 }
