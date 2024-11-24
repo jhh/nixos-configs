aa69fc9558ac8cbe6f66094e3ee07d61a1af4bc5
Jeff Hutchison <jeff@jeffhutchison.com>
2024-11-24 10:49:41 -0500

    Update samba config for 24.11

--- hosts/phobos/samba.nix
+++ hosts/phobos/samba.nix
@@ -10,38 +10,34 @@
 
   services.samba = {
     enable = true;
-    securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = Phobos
-      netbios name = PHOBOS
-      security = user
-      min protocol = SMB2
-      ea support = yes
+    settings.global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "Phobos";
+      "netbios name" = "PHOBOS";
+      "security" = "user";
+      "min protocol" = "SMB2";
+      "ea support" = "yes";
+      "logging" = "0";
 
-      vfs objects = fruit streams_xattr
-      fruit:aapl = yes
-      fruit:metadata = stream
-      fruit:model = MacSamba
-      fruit:veto_appledouble = no
-      fruit:posix_rename = yes
-      fruit:zero_file_id = yes
-      fruit:wipe_intentionally_left_blank_rfork = yes
-      fruit:delete_empty_adfiles = yes
+      "vfs objects" = "fruit streams_xattr";
+      "fruit:aapl" = "yes";
+      "fruit:metadata" = "stream";
+      "fruit:model" = "MacSamba";
+      "fruit:veto_appledouble" = "no";
+      "fruit:nfs_aces" = "no";
+      "fruit:wipe_intentionally_left_blank_rfork" = "yes";
+      "fruit:delete_empty_adfiles" = "yes";
+      "fruit:posix_rename" = "yes";
 
-      use sendfile = yes
-      socket options = IPTOS_LOWDELAY TCP_NODELAY
-      hosts allow = 192.168.3.0/24 100.64.0.0/10 100.69.0.0/10
-      guest account = nobody
-      map to guest = bad user
+      "hosts allow" = [ "192.168.3.0/24" "100.64.0.0/10" "100.69.0.0/10" ];
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
 
-      load printers = no
-      printing = bsd
-      printcap name = /dev/null
-      disable spoolss = yes
-    '';
+      "load printers" = "no";
+    };
 
-    shares = {
+
+    settings = {
       StrykeForce = {
         path = "/mnt/tank/share/strykeforce";
         public = "yes";
