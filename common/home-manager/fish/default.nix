adbf35aa7a5b4c772c119994ca8e2019df3c6fe5
Jeff Hutchison <jeff@jeffhutchison.com>
2024-12-28 06:45:47 -0500

    Add tokyonight fish shell colors

--- common/home-manager/fish/default.nix
+++ common/home-manager/fish/default.nix
@@ -62,22 +62,45 @@ in
           direnv hook fish | source
       end
 
-      set --local WPI_YEAR 2024
-      if test -d ~/wpilib/$WPI_YEAR
-          abbr --add --global pathweaver ~/wpilib/$WPI_YEAR/tools/PathWeaver.py
-          abbr --add --global outlineviewer ~/wpilib/$WPI_YEAR/tools/OutlineViewer.py
-          set --export JAVA_HOME ~/wpilib/$WPI_YEAR/jdk
-          fish_add_path $JAVA_HOME/bin
-      end
-
       if test -x /opt/homebrew/bin/brew
           eval (/opt/homebrew/bin/brew shellenv)
       end
 
-      if test -x ~/.ghcup/bin
-          fish_add_path ~/.ghcup/bin
-      end
+          # TokyoNight Color Palette
+      set -l foreground c0caf5
+      set -l selection 283457
+      set -l comment 565f89
+      set -l red f7768e
+      set -l orange ff9e64
+      set -l yellow e0af68
+      set -l green 9ece6a
+      set -l purple 9d7cd8
+      set -l cyan 7dcfff
+      set -l pink bb9af7
 
+      # Syntax Highlighting Colors
+      set -g fish_color_normal $foreground
+      set -g fish_color_command $cyan
+      set -g fish_color_keyword $pink
+      set -g fish_color_quote $yellow
+      set -g fish_color_redirection $foreground
+      set -g fish_color_end $orange
+      set -g fish_color_option $pink
+      set -g fish_color_error $red
+      set -g fish_color_param $purple
+      set -g fish_color_comment $comment
+      set -g fish_color_selection --background=$selection
+      set -g fish_color_search_match --background=$selection
+      set -g fish_color_operator $green
+      set -g fish_color_escape $pink
+      set -g fish_color_autosuggestion $comment
+
+      # Completion Pager Colors
+      set -g fish_pager_color_progress $comment
+      set -g fish_pager_color_prefix $cyan
+      set -g fish_pager_color_completion $foreground
+      set -g fish_pager_color_description $comment
+      set -g fish_pager_color_selected_background --background=$selection
     '';
   };
 
