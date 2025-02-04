54f16ed2d946431c8f03b67a706ac706011b0f0b
Jeff Hutchison <jeff@jeffhutchison.com>
2025-02-04 10:28:30 -0500

    Get styx booting and able to ssh into

--- hosts/styx/configuration.nix
+++ hosts/styx/configuration.nix
@@ -1,6 +1,7 @@
 {
-  config,
-  lib,
+  flake,
+  inputs,
+  perSystem,
   pkgs,
   ...
 }:
@@ -8,16 +9,27 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./postgresql.nix
-    ./todo.nix
+    inputs.srvos.nixosModules.server
+    # ./postgresql.nix
+    # ./todo.nix
   ];
 
-  j3ff = {
-    mail.enable = true;
-    tailscale.enable = false;
-    man.enable = true;
-    mdns.enable = true;
+  environment.systemPackages = [ pkgs.ghostty.terminfo ];
+
+  users.users.root = {
+    # https://start.1password.com/open/i?a=7Z533SZAYZCNVL764G5INOV75Q&v=lwpxghrefna57cr6nw7mr3bybm&i=v6cyausjzre6hjypvdsfhlkbty&h=my.1password.com
+    hashedPassword = "$y$j9T$6B8V0Z9VkFiU0fMwSuLrA0$z3YHuwwAZro3N7TopVIsNltIJ5BXt3TQj1wQqt5HSuD";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqpWpNJzfzioGYyR9q4wLwPkBrnmc/Gdl6JsO+SUpel jeff@j3ff.io"
+    ];
   };
+
+  # j3ff = {
+  #   mail.enable = true;
+  #   tailscale.enable = false;
+  #   man.enable = true;
+  #   mdns.enable = true;
+  # };
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
@@ -52,7 +64,6 @@
       "8.8.8.8"
       "8.8.4.4"
     ];
-    firewall.enable = false;
   };
 
   system.stateVersion = "24.05";
