d416a375a2bfe1a636a4c40c18747bf7c91c4733
Jeff Hutchison <jeff@jeffhutchison.com>
2025-02-06 17:25:13 +0000

    Add paperless user to servers

--- modules/nixos/server-j3ff.nix
+++ modules/nixos/server-j3ff.nix
@@ -96,4 +96,20 @@
       gid = 994;
     };
   };
+
+  age.secrets.paperless_passwd = {
+    file = ../../secrets/paperless_passwd.age;
+  };
+
+  users.users.paperless = {
+    isSystemUser = true;
+    uid = config.ids.uids.paperless;
+    home = config.services.paperless.dataDir;
+    group = "paperless";
+    hashedPasswordFile = config.age.secrets.paperless_passwd.path;
+  };
+
+  users.groups.paperless = {
+    gid = config.ids.gids.paperless;
+  };
 }
