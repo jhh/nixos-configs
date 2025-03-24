91f26493e56da10c65fd62d6c062849cff0fa163
Jeff Hutchison <jeff@jeffhutchison.com>
2025-03-24 12:48:22 -0400

    Enable linux-builder for NixOS tests on darwin

--- hosts/ganymede/darwin-configuration.nix
+++ hosts/ganymede/darwin-configuration.nix
@@ -7,5 +7,19 @@
   ];
 
   nixpkgs.hostPlatform = "aarch64-darwin";
+  nix.linux-builder = {
+    enable = true;
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+  };
   system.stateVersion = 4;
 }
