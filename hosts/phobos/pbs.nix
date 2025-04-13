4d13dffb994313032dee6c5ce761f8da4d5c1837
Jeff Hutchison <jeff@jeffhutchison.com>
2025-04-13 13:39:21 +0000

    Back up other critical NixOS state

--- hosts/phobos/pbs.nix
+++ hosts/phobos/pbs.nix
@@ -9,13 +9,25 @@
     file = ../../secrets/pbs_gembrit.age;
   };
 
+  environment.etc.".pxarexclude".text = ''
+    **/*
+    !group
+    !machine-id
+    !passwd
+    !subgid
+  '';
+
   systemd.services.pbs-backup = {
     startAt = "daily";
 
     serviceConfig = {
       EnvironmentFile = config.age.secrets.pbs_gembrit.path;
       ExecStart = ''
-        ${lib.getExe pkgs.proxmox-backup-client} backup strykeforce.pxar:/mnt/tank/share/strykeforce --ns daily
+        ${lib.getExe pkgs.proxmox-backup-client} backup \
+          strykeforce.pxar:/mnt/tank/share/strykeforce \
+          nixos.pxar:/var/lib/nixos \
+          etc.pxar:/etc \
+          --ns daily
       '';
     };
   };
