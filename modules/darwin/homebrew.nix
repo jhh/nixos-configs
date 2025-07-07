3bbf7ea50ba61f25a2ea6aafe2316d4bc328d673
Jeff Hutchison <jeff@jeffhutchison.com>
2025-07-07 08:24:18 -0400

    Remove docker, re-enable mas apps on darwin

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -20,7 +20,6 @@
       "carbon-copy-cloner"
       "dash"
       "discord"
-      "docker"
       "firefox"
       "iterm2"
       "jetbrains-toolbox"
@@ -37,14 +36,14 @@
       "zotero"
     ];
 
-    # masApps = {
-    #   "Icon Slate" = 439697913;
-    #   "Infuse 7" = 1136220934;
-    #   "Paprika Recipe Manager 3" = 1303222628;
-    #   "Pcalc" = 403504866;
-    #   "Reeder 5" = 1529448980;
-    #   "Slack for Desktop" = 803453959;
-    #   "Tailscale" = 1475387142;
-    # };
+    masApps = {
+      "Icon Slate" = 439697913;
+      "Infuse 7" = 1136220934;
+      "Paprika Recipe Manager 3" = 1303222628;
+      "Pcalc" = 403504866;
+      "Reeder 5" = 1529448980;
+      "Slack for Desktop" = 803453959;
+      "Tailscale" = 1475387142;
+    };
   };
 }
