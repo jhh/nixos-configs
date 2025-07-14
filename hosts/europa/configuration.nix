04bea573a4ebbeb032f656317cc9b15ee328d8cc
Jeff Hutchison <jeff@jeffhutchison.com>
2025-07-14 17:15:13 -0400

    europa: removed commented out config

--- hosts/europa/configuration.nix
+++ hosts/europa/configuration.nix
@@ -42,32 +42,6 @@
     '';
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # users.users.jeff = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  #   home = "/home/jeff";
-  #   description = "Jeff Hutchison";
-  #   extraGroups = [
-  #     "docker"
-  #     "wheel"
-  #   ];
-  #   hashedPassword = "$6$Iz7OA82lRmO$6SqGFySdF4gr8U47sIY6Vf.WzVJjtrZ4hiGQ1OPCpksEvj4Uo5.ylfI1czif0o488BcHXGIlDIpnJY3kIgQeT0";
-  #   openssh.authorizedKeys.keys = [
-  #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqpWpNJzfzioGYyR9q4wLwPkBrnmc/Gdl6JsO+SUpel jeff@j3ff.io"
-  #   ];
-  # };
-  #
-  # users.users.root = {
-  #   # https://start.1password.com/open/i?a=7Z533SZAYZCNVL764G5INOV75Q&v=lwpxghrefna57cr6nw7mr3bybm&i=v6cyausjzre6hjypvdsfhlkbty&h=my.1password.com
-  #   hashedPassword = "$6$fQkrR0Y/UOZ7$RG1ARltUwRE2Q/zSM88en2KNpx2gDfzV/PSqjCq/c3njspl62.h6HFPnk1L.b8UvSWnxvoew/r79/CxwfzUYW1";
-  #   openssh.authorizedKeys.keys = config.users.users.jeff.openssh.authorizedKeys.keys;
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [
     brave
@@ -91,24 +65,9 @@
   services.zfs.autoScrub.enable = true;
   services.zfs.autoScrub.pools = [ "rpool" ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
