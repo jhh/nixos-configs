c669418587d6fa3cc518d9e58f8d6e41db4efeb0
Jeff Hutchison <jeff@jeffhutchison.com>
2025-07-19 16:33:15 -0400

    home-manager: add enable option for neovim

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -1,53 +1,65 @@
-{ lib, pkgs, ... }:
-lib.mkIf pkgs.stdenv.isLinux {
-  programs.neovim = {
-    # package = specialArgs.inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    defaultEditor = true;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.j3ff.nvim;
+in
+{
+  options.j3ff.nvim.enable = lib.mkEnableOption "Neovim";
 
-    plugins = with pkgs.vimPlugins; [
-      dash-vim
-      formatter-nvim
-      lazygit-nvim
-      mini-nvim
-      nvim-lspconfig
-      nvim-treesitter.withAllGrammars
-      nvim-web-devicons
-      oil-nvim
-      tailwind-tools-nvim
-      telescope-file-browser-nvim
-      telescope-fzf-native-nvim
-      telescope-nvim
-      telescope-undo-nvim
-      tokyonight-nvim
-      which-key-nvim
-    ];
+  config = lib.mkIf cfg.enable {
+    programs.neovim = {
+      # package = specialArgs.inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      defaultEditor = true;
 
-    extraPackages = with pkgs; [
-      basedpyright
-      fd
-      lua-language-server
-      nil
-      nixfmt-rfc-style
-      ripgrep
-      ruff
-      stylua
-      tailwindcss-language-server
-    ];
+      plugins = with pkgs.vimPlugins; [
+        dash-vim
+        formatter-nvim
+        lazygit-nvim
+        mini-nvim
+        nvim-lspconfig
+        nvim-treesitter.withAllGrammars
+        nvim-web-devicons
+        oil-nvim
+        tailwind-tools-nvim
+        telescope-file-browser-nvim
+        telescope-fzf-native-nvim
+        telescope-nvim
+        telescope-undo-nvim
+        tokyonight-nvim
+        which-key-nvim
+      ];
 
-    extraLuaConfig =
-      let
-        initLua = pkgs.concatText "init.lua" [
-          ./fold.lua
-          ./formatter.lua
-          ./init.lua
-          ./lsp.lua
-          ./telescope.lua
-        ];
-      in
-      builtins.readFile initLua;
+      extraPackages = with pkgs; [
+        basedpyright
+        fd
+        lua-language-server
+        nil
+        nixfmt-rfc-style
+        ripgrep
+        ruff
+        stylua
+        tailwindcss-language-server
+      ];
+
+      extraLuaConfig =
+        let
+          initLua = pkgs.concatText "init.lua" [
+            ./fold.lua
+            ./formatter.lua
+            ./init.lua
+            ./lsp.lua
+            ./telescope.lua
+          ];
+        in
+        builtins.readFile initLua;
+    };
   };
 }
