d583b08f2ab24359340789ce356e30758af8f299
Jeff Hutchison <jeff@jeffhutchison.com>
2026-02-19 08:05:35 -0700

    use ssh to sign git commits, reorganized configs

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -1,6 +1,21 @@
-{ ... }:
+{ config, ... }:
 let
   gitConfig = {
+    alias = {
+      amend = "commit --amend -m";
+      br = "branch";
+      co = "checkout";
+      st = "status";
+      la = "!git config -l | grep alias | cut -c 7-";
+      ls = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --all --decorate --oneline --graph'';
+      ll = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate --numstat'';
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %s %C(italic)- %an%C(reset)%C(magenta bold)%d%C(reset)' --all";
+
+      cm = "commit -m";
+      ca = "commit -am";
+      dc = "diff --cached";
+    };
+
     core.editor = "vim";
     github.user = "jhh";
 
@@ -42,36 +57,33 @@ let
     mergetool.nvimdiff.cmd = ''nvim -d "$LOCAL" "$REMOTE" "$MERGED" -c "$wincmd w" -c "wincmd J"'';
     mergetool.prompt = false;
     mergetool.keepBackup = false;
+
+    user = {
+      name = "Jeff Hutchison";
+      email = "jeff@jeffhutchison.com";
+    };
+
+    gitbutler.signCommits = true;
+    gpg.ssh.allowedSignersFile = "${config.xdg.configHome}/git/allowed_signers";
   };
 
 in
 {
   programs.diff-so-fancy.enable = true;
-  programs.git.enable = true;
-  programs.git.settings = {
-    user.name = "Jeff Hutchison";
-    user.email = "jeff@jeffhutchison.com";
+  programs.git = {
+    enable = true;
+    settings = gitConfig;
     signing = {
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
       signByDefault = true;
-      key = "26960A62CBEEC91D";
     };
+    ignores = import ./ignore.nix;
+  };
 
-    alias = {
-      amend = "commit --amend -m";
-      br = "branch";
-      co = "checkout";
-      st = "status";
-      la = "!git config -l | grep alias | cut -c 7-";
-      ls = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --all --decorate --oneline --graph'';
-      ll = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate --numstat'';
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %s %C(italic)- %an%C(reset)%C(magenta bold)%d%C(reset)' --all";
-
-      cm = "commit -m";
-      ca = "commit -am";
-      dc = "diff --cached";
-    };
-  }
-  // gitConfig;
-
-  programs.git.ignores = import ./ignore.nix;
+  xdg.configFile."git/allowed_signers" = {
+    text = ''
+      jeff@jeffhutchison.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPqpWpNJzfzioGYyR9q4wLwPkBrnmc/Gdl6JsO+SUpel
+    '';
+  };
 }
