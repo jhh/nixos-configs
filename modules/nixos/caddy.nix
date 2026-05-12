020c8b7c37db46367fa70640c723b4878b4f3ad0
Jeff Hutchison <jeff@j3ff.io>
2026-05-12 13:00:17 +0000

    caddy: update plugin SHA

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -10,7 +10,7 @@
     # https://wiki.nixos.org/wiki/Caddy#Plug-ins
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/dnsimple@v0.0.0-20251214142352-69317c3989f0" ];
-      hash = "sha256-FdTcJu3iqZuHK/SRvW57YTSKxeCF9gWMhRHECCO4FnY=";
+      hash = "sha256-9apjvIoXyOtKSpS6DPU7YgedchW+1WcmSwOV+FRt2zo=";
     };
 
     environmentFile = config.age.secrets.dnsimple_secrets.path;
